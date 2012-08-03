4fc7d65af04233d2bef8b8c7813a978a6596aab0
Lazerdong <couten@lexile.com>
2012-08-03 14:06:41 -0400

    Added jsonrpc and added to installed apps

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = (
     'djangotoolbox',
     'autoload',
     'dbindexer',
-
+    'jsonrpc',
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
 )
